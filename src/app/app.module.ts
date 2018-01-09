4fa5000ec8879e034f4b7b6e811a46828c220ba6
deniskorotin12 <deniskorotin4@gmail.com>
2018-01-09 19:59:37 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
+import { AppRoutingModule } from './app-routing.module';
+import { UsersService } from './shared/services/users.service';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    AuthModule,
+    AppRoutingModule
+  ],
+  providers: [UsersService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
